cae79d3584f94a6a08083064170764cfe127b4c7
Seonggoo Kang <ksung91889@naver.com>
2023-02-19 17:19:04 +0900

    WIP

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes['main-image']}> {/*  - 가 들어가므로 점표기법 안하고 []로 감싸줘야함*/}
+        <img src={mealsImage} alt="A table full of delicious food!"/>
+      </div>
+    </Fragment>
   )
 }
 
